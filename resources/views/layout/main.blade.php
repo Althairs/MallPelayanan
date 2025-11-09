7e682c9f04c32936caf224adf64a76b2f91c0b91
Althairs <148575542+Althairs@users.noreply.github.com>
2025-11-09 19:07:30 +0800

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BXaKDRCW.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
+    <!-- <link rel="stylesheet" href="{{ asset('build/assets/app-BXaKDRCW.css') }}"> -->
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -19,7 +18,7 @@
     @yield('kontent')
     @include('layout.footer')
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-    <script src="{{ asset('build/assets/app-CAiCLEjY.js') }}"></script>
+    <!-- <script src="{{ asset('build/assets/app-CAiCLEjY.js') }}"></script> -->
 </body>
 
 </html>
