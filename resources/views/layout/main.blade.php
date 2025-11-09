78216e4eb6c88531186e6e05f42e2eddc106cf1a
Angga <satriaindradewangga@gmail.com>
2025-11-09 18:45:43 +0800

    vercel again

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BXaKDRCW.css') }}">
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
@@ -17,6 +19,7 @@
     @yield('kontent')
     @include('layout.footer')
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <script src="{{ asset('build/assets/app-CAiCLEjY.js') }}"></script>
 </body>
 
 </html>
